beaef20ce90e1798884ffed3e433af9d16f565d4
Manoj Ahirwar <manoj.ahirwar6991@gmail.com>
2022-02-07 15:49:45 +0530

    First commit

---
+++ src/test/java/com/qa/runners/MyPixel2XLTestNGRunnerTest.java
@@ -0,0 +1,23 @@
+package com.qa.runners;
+
+import io.cucumber.testng.CucumberOptions;
+
+/**
+ * An example of using TestNG when the test class does not inherit from
+ * AbstractTestNGCucumberTests but still executes each scenario as a separate
+ * TestNG test.
+ */
+@CucumberOptions(
+        plugin = {"pretty"
+        , "html:target/Pixel2XL/cucumber"
+        , "summary"
+        , "me.jvt.cucumber.report.PrettyReports:target/Pixel3/cucumber-html-reports"}
+        ,features = {"src/test/resources"}
+        ,glue = {"com.qa.stepdef"}
+        ,dryRun=false
+        ,monochrome=true
+        ,tags = "@test"
+        )
+public class MyPixel2XLTestNGRunnerTest extends RunnerBase {
+
+}
